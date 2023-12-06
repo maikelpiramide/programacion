1e757d4924c7a4b5278924fcbbae12f41bbbe9cc
miguel bautista <230254@fppiramide.com>
2023-12-06 20:49:14 +0100

    initial commit Ejercicio5 UD4

---
+++ UD4/Ejercicio5/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+        Cancion cancion1 = new Cancion();
+        Cancion cancion2 = new Cancion("Through da storm","poloG");
+
+        cancion1.dameTitulo();
+        cancion1.dameAutor();
+
+        cancion2.dameTitulo();
+        cancion2.dameAutor();
+
+        cancion1.ponAutor("Pop smoke");
+        cancion1.ponTitulo("Dior");
+
+        cancion1.dameTitulo();
+        cancion1.dameAutor();
+
+    }
+}
