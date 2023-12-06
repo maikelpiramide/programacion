c32a93c37de0463ac6a4c4f5b09a0ee05ca88704
miguel bautista <230254@fppiramide.com>
2023-12-06 20:18:51 +0100

    ejercicio 4 UD4 clase Cancion terminada

---
+++ UD4/Ejercicio4/src/Cancion.java
@@ -0,0 +1,31 @@
+public class Cancion {
+    private String titulo;
+    private String autor;
+
+    public Cancion(String titulo,String autor){
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+    public Cancion(){
+        this.titulo = "";
+        this.autor = "";
+    }
+
+    public void dameTitulo(){
+        System.out.println("-----------");
+        System.out.println(this.titulo);
+        System.out.println("-----------");
+    }
+    public void dameAutor(){
+        System.out.println("-----------");
+        System.out.println(this.autor);
+        System.out.println("-----------");
+    }
+
+    public void ponTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    public void ponAutor(String autor){
+        this.autor = autor;
+    }
+}
