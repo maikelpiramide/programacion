de21d554ed5f2d7c763fc8078a3b99a6f461dff4
miguel bautista <230254@fppiramide.com>
2023-12-06 20:30:32 +0100

    objetos creados en el main

--- UD4/Ejercicio4/src/App.java
+++ UD4/Ejercicio4/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) {
-
+        Cancion cancion1 = new Cancion();
+        Cancion cancion2 = new Cancion("Through da storm","poloG");
+        
     }
 }
