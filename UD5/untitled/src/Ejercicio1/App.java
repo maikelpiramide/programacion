8ac3b1f73f0997399a360cee716ef3aad2080975
Miguel Bautista <230254@fppiramide.com>
2024-01-25 13:49:48 +0100

    Unidad5 initial commit

---
+++ UD5/untitled/src/Ejercicio1/App.java
@@ -0,0 +1,11 @@
+package Ejercicio1;
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        String dia = JOptionPane.showInputDialog("Introduce el dia");
+        String mes = JOptionPane.showInputDialog("Introduce el mes");
+        String anno = JOptionPane.showInputDialog("Introduce el año");
+
+    }
+}
