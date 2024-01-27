dc84d84fba20a458d257c770576df372ac175620
miguel bautista <230254@fppiramide.com>
2024-01-27 22:09:13 +0100

    sincro pc

---
+++ ExplicacionClase/PruebasCollection/src/App.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        Collection<String> list;
+        list = new ArrayList<>();
+        list.add("dato01");
+        list.add("dato02");
+        list.forEach(elemento -> {
+            System.out.println(elemento);
+        });
+        list = new TreeSet<>();
+        list.add("dato04");
+        list.add("dato05");
+        for (int i = 0; i < list.)
+
+    }
+}
