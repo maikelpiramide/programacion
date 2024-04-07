c824f25d88fb45ce69191bf05102e2e645e051e6
miguel bautista <230254@fppiramide.com>
2024-04-07 18:20:37 +0200

    ejercicio 1 swing terminado

--- UD8/src/Ejercicio1/App.java
+++ UD8/src/Ejercicio1/App.java
@@ -7,24 +7,26 @@ public class App {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Cálculo de triángulo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLa
-        frame.setSize(300,100);
+
+        frame.setSize(300,150);
         frame.setResizable(false);
 
         JLabel nivel = new JLabel("base");
         JTextField base = new JTextField(20);
         JLabel nivAltura = new JLabel("Altura");
         JTextField altura = new JTextField(20);
-        JButton boton = new JButton("pruebas");
-
+        JButton boton = new JButton("Calcular");
+        JLabel resultado = new JLabel("");
         JPanel panel = new JPanel();
         panel.add(nivel);
         panel.add(base);
         panel.add(nivAltura);
         panel.add(altura);
         panel.add(boton);
+        panel.add(resultado);
         frame.add(panel);
         frame.setVisible(true);
-        boton.addActionListener(new Escuchador());
+
+        boton.addActionListener(new Escuchador(base,altura,resultado));
     }
 }
--- UD8/src/Ejercicio1/Escuchador.java
+++ UD8/src/Ejercicio1/Escuchador.java
@@ -1,12 +1,29 @@
 package Ejercicio1;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Escuchador implements ActionListener {
-
+    private JTextField base;
+    private JTextField altura;
+    private JLabel resultado;
+    Escuchador(JTextField base, JTextField altura,JLabel resultado){
+        this.base = base;
+        this.altura = altura;
+        this.resultado = resultado;
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        System.out.println(e);
+        try{
+            double base = Double.parseDouble(this.base.getText());
+            double altura = Double.parseDouble(this.altura.getText());
+            double respuesta = (base * altura)/2;
+            System.out.println(respuesta);
+            this.resultado.setText("Area = " + String.valueOf(respuesta));
+        }catch(Exception ex){
+            this.resultado.setText("<html><span style='color:red;'>Introduce valores corresctos.<br>Como enteros o decimales </span></html>");
+        }
     }
 }
